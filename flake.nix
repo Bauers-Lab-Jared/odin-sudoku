5545c3be270658a4df1849cc27640fffee468017
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-05-23 06:12:11 -0400

    Added nvim to devShell and updated flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,28 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nvim.url = "github:bauers-lab-jared/nixvim";
+  };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    ...
   }: let
     out = system: let
       pkgs = nixpkgs.legacyPackages.${system};
       appliedOverlay = self.overlays.default pkgs pkgs;
     in {
       packages.default = appliedOverlay.default;
+      devShells.default = pkgs.mkShell {
+        inherit (appliedOverlay.default) nativeBuildInputs buildInputs;
+
+        packages = [
+          self.inputs.nvim.packages.${system}.nvim-odin
+        ];
+      };
     };
   in
     flake-utils.lib.eachDefaultSystem out
