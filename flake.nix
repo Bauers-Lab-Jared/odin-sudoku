cd9a42430cf41c7a0071d7309cf188cb8dce43c0
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-05-31 00:52:55 -0400

    updated nixvim with better direnv loading

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixvim.url = "github:bauers-lab-jared/nixvim/flake-restructure";
+    nixvim.url = "github:bauers-lab-jared/nixvim";
   };
 
   outputs = {
@@ -19,23 +19,8 @@
       devShells.default = pkgs.mkShell {
         inherit (appliedOverlay.default) nativeBuildInputs buildInputs;
 
-        packages = let
-          inherit (self.inputs.nixvim) packages nixosModules;
-          username = nixpkgs.lib.removeSuffix "\n" (builtins.readFile ./.user);
-
-          nixvim-base =
-            if username != ""
-            then packages.${system}.nixvim.nixvimExtend nixosModules."user-${username}"
-            else packages.${system}.nixvim;
-
-          nixvim =
-            (nixvim-base.nixvimExtend nixosModules.proj-odin).nixvimExtend
-            {
-              # project specific config here
-            };
-        in [
-          nixvim
-        ];
+        packages = [(self.inputs.nixvim.lib.mkNixvim {inherit pkgs; addons =
+        [ "proj-odin" "proj-nix" ];})];
       };
     };
   in
