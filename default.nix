9485e1b976df2e8537729b5d1d77e68ecfd93ed5
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-09-09 15:58:25 -0400

    WIP trying to fix odin linking to things

--- default.nix
+++ default.nix
@@ -4,6 +4,9 @@
   go-task,
   qqwing,
   gdb,
+  libGL,
+  xorg,
+  autoPatchelfHook,
 }:
 stdenv.mkDerivation rec {
   pname = "odin-sudoku";
@@ -13,17 +16,32 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [
     gdb
     go-task
-    odin
     qqwing
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    libGL
+    xorg.libX11
+    odin
   ];
 
   buildPhase = ''
+    runHook preBuild
+
     odin build ./main/ \
-    -out:${pname}
+    -out:${pname} \
+    -extra-linker-flags:"-l:${odin}/share/vendor/raylib/linux/libraylib.a"
+
+    runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     cp ${pname} $out/bin
+
+    runHook postInstall
   '';
 }
