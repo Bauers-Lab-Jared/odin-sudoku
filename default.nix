f908ff16f4571459ccbe813c65a9ace0b2a14219
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-09-10 10:58:17 -0400

    WIP

--- default.nix
+++ default.nix
@@ -1,37 +1,34 @@
 {
-  stdenv,
+  clangStdenv,
   odin,
   go-task,
   qqwing,
   gdb,
   libGL,
   xorg,
-  autoPatchelfHook,
 }:
-stdenv.mkDerivation rec {
+clangStdenv.mkDerivation rec {
   pname = "odin-sudoku";
   version = "0.1";
   src = ./src;
 
   nativeBuildInputs = [
-    gdb
     go-task
     qqwing
-    autoPatchelfHook
+    odin
+    gdb
   ];
 
   buildInputs = [
     libGL
     xorg.libX11
-    odin
   ];
 
   buildPhase = ''
     runHook preBuild
 
-    odin build ./main/ \
+    ${odin}/bin/odin build ./main/ \
     -out:${pname} \
-    -extra-linker-flags:"-l:${odin}/share/vendor/raylib/linux/libraylib.a"
 
     runHook postBuild
   '';
